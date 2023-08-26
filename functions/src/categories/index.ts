65d53a159be397a12a045861cf020157e7fa41a0
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2023-08-26 09:22:56 -0600

    remove unused

--- functions/src/categories/index.ts
+++ functions/src/categories/index.ts
@@ -4,54 +4,55 @@ import { getFirestore } from "firebase-admin/firestore";
 import _ = require("lodash");
 //import { v4 as uuidv4 } from 'uuid';
 
-const db = getFirestore()
+const db = getFirestore();
 
-exports.onCreateCategory = functions.firestore.document('categories/{documentId}').onCreate(async (snapshot, context) => {
+exports.onCreateCategory = functions.firestore
+  .document("categories/{documentId}")
+  .onCreate(async (snapshot, context) => {
     try {
-        const documentId = context.params.documentId;
-       // const data = snapshot.data();
-        const newData = {
-            created_at: new Date(),
-            updated_at: new Date(),
-            count_courses: 0,
-            highlight: 0,
-            count_views: 0,
-        }
-        try {
-            await db.collection('categories').doc(documentId).update(newData);
-        } catch (e) {
-            logger.info("no se crearon los campos", e)
-        }
-
-       
-
-    } catch (e) {
-        logger.info("No se asignaron los datoss", e)
-    }
-})
-
-exports.onUpdateCategory = functions.firestore.document('categories/{documentId}').onUpdate(async (snapshot, context) => {
-    const excludeFields = (obj: any) => {
-        const { updated_at, ...rest } = obj
-        return rest
+      const documentId = context.params.documentId;
+      // const data = snapshot.data();
+      const newData = {
+        created_at: new Date(),
+        updated_at: new Date(),
+        count_courses: 0,
+        highlight: 0,
+        count_views: 0,
+      };
+      try {
+        await db.collection("categories").doc(documentId).update(newData);
+      } catch (e) {
+        logger.info("no se crearon los campos", e);
       }
+    } catch (e) {
+      logger.info("No se asignaron los datoss", e);
+    }
+  });
+
+exports.onUpdateCategory = functions.firestore
+  .document("categories/{documentId}")
+  .onUpdate(async (snapshot, context) => {
+    const excludeFields = (obj: any) => {
+      const { updated_at, ...rest } = obj;
+      return rest;
+    };
     try {
-        const documentId = context.params.documentId;
-        const beforeData = excludeFields(snapshot.before.data());
-        const afterData = excludeFields(snapshot.after.data());
-  
-        if(_.isEqual(beforeData, afterData)){
-          return;
-        }
-  
-        const newUpdateDate = {       
-            updated_at: new Date(),
-        }
-        try {
-            await db.collection('categories').doc(documentId).update(newUpdateDate);
-        } catch (e) {
-            logger.info("no se crearon los campos", e)
-        }
+      const documentId = context.params.documentId;
+      const beforeData = excludeFields(snapshot.before.data());
+      const afterData = excludeFields(snapshot.after.data());
+
+      if (_.isEqual(beforeData, afterData)) {
+        return;
+      }
+
+      const newUpdateDate = {
+        updated_at: new Date(),
+      };
+      try {
+        await db.collection("categories").doc(documentId).update(newUpdateDate);
+      } catch (e) {
+        logger.info("no se crearon los campos", e);
+      }
     } catch (e) {
       logger.info("No se asignaron los datoss", e);
     }
--- functions/src/points/index.ts
+++ functions/src/points/index.ts
@@ -1,105 +1,133 @@
-
 import functions = require("firebase-functions");
 import { getFirestore } from "firebase-admin/firestore";
 import dayjs = require("dayjs");
+
 const db = getFirestore();
 
+const getLeftPoints = async (userId) => {
+  let recalculate = 0;
+  const docRef = await db
+    .collection("users")
+    .doc(userId)
+    .collection("left-points")
+    .get();
+  docRef.forEach((d) => {
+    recalculate += d.data().points;
+  });
+  return recalculate;
+};
+
+const getRightPoints = async (userId) => {
+  let recalculate = 0;
+  const docRef = await db
+    .collection("users")
+    .doc(userId)
+    .collection("right-points")
+    .get();
+  docRef.forEach((d) => {
+    recalculate += d.data().points;
+  });
+  return recalculate;
+};
+
 //funcion que detecta cuando se generan puntos a la izquierda con fecha de expiracion
-exports.onCreateLeftPoints = functions.firestore.document('users/{userId}/left-points/{sideId}').onCreate(async (snapshot, context) => {
+exports.onCreateLeftPoints = functions.firestore
+  .document("users/{userId}/left-points/{sideId}")
+  .onCreate(async (snapshot, context) => {
     const userId = context.params.userId;
-    const sideId = context.params.sideId
+    const sideId = context.params.sideId;
     const addDefaultFields = async () => {
-        const newData = {
-            starts_at: new Date(),
-            expires_at: dayjs(new Date()).add(84, 'day').toDate()
-        }
-        try {
-            await db.collection('users').doc(userId).collection('left-points').doc(sideId).update(newData);
-        } catch (e) {
-            functions.logger.log("fallo al agregar los datos default", e)
-        }
-    }
+      const newData = {
+        starts_at: new Date(),
+        expires_at: dayjs(new Date()).add(84, "day").toDate(),
+      };
+      try {
+        await db
+          .collection("users")
+          .doc(userId)
+          .collection("left-points")
+          .doc(sideId)
+          .update(newData);
+      } catch (e) {
+        functions.logger.log("fallo al agregar los datos default", e);
+      }
+    };
     const updateBinary = async () => {
-        await addDefaultFields();
-        let recalculate = 0;
-        const docRef = await db.collection('users').doc(userId).collection('left-points').get()
-        docRef.forEach((d) => {
-            recalculate += d.data().points
-        })
-        try {
-            await db.collection('users').doc(userId).update({
-                'left_points': recalculate
-            })
-        } catch (e) {
-            functions.logger.log("fallo al actualizar los puntos", e)
-        }
-    }
+      await addDefaultFields();
 
-    await updateBinary()
+      try {
+        const left_points = await getLeftPoints(userId);
+        await db.collection("users").doc(userId).update({
+          left_points,
+        });
+      } catch (e) {
+        functions.logger.log("fallo al actualizar los puntos", e);
+      }
+    };
 
-})
+    await updateBinary();
+  });
 
-exports.onDeleteLeftPoints = functions.firestore.document('users/{userId}/left-points/{sideId}').onDelete(async (snapshot, context) => {
+exports.onDeleteLeftPoints = functions.firestore
+  .document("users/{userId}/left-points/{sideId}")
+  .onDelete(async (snapshot, context) => {
     const userId = context.params.userId;
-    let recalculate = 0;
-    const docRef = await db.collection('users').doc(userId).collection('left-points').get()
-    docRef.forEach((d) => {
-        recalculate += d.data().points
-    })
     try {
-        await db.collection('users').doc(userId).update({
-            'left_points': recalculate
-        })
+      const left_points = await getLeftPoints(userId);
+      await db.collection("users").doc(userId).update({
+        left_points,
+      });
     } catch (e) {
-        functions.logger.log("fallo al actualizar los puntos", e)
+      functions.logger.log("fallo al actualizar los puntos", e);
     }
-})
+  });
 
-exports.onCreateRightPoints = functions.firestore.document('users/{userId}/right-points/{sideId}').onCreate(async (snapshot, context) => {
+exports.onCreateRightPoints = functions.firestore
+  .document("users/{userId}/right-points/{sideId}")
+  .onCreate(async (snapshot, context) => {
     const userId = context.params.userId;
-    const sideId = context.params.sideId
+    const sideId = context.params.sideId;
     const addDefaultFields = async () => {
-        const newData = {
-            starts_at: new Date(),
-            expires_at: dayjs(new Date()).add(84, 'day').toDate()
-        }
-        try {
-            await db.collection('users').doc(userId).collection('right-points').doc(sideId).update(newData);
-        } catch (e) {
-            functions.logger.log("fallo al agregar los datos default", e)
-        }
-    }
+      const newData = {
+        starts_at: new Date(),
+        expires_at: dayjs(new Date()).add(84, "day").toDate(),
+      };
+      try {
+        await db
+          .collection("users")
+          .doc(userId)
+          .collection("right-points")
+          .doc(sideId)
+          .update(newData);
+      } catch (e) {
+        functions.logger.log("fallo al agregar los datos default", e);
+      }
+    };
     const updateBinary = async () => {
-        await addDefaultFields();
-        let recalculate = 0;
-        const docRef = await db.collection('users').doc(userId).collection('right-points').get()
-        docRef.forEach((d) => {
-            recalculate += d.data().points
-        })
-        try {
-            await db.collection('users').doc(userId).update({
-                'right_points': recalculate
-            })
-        } catch (e) {
-            functions.logger.log("fallo al actualizar los puntos", e)
-        }
-    }
+      await addDefaultFields();
+      try {
+        const right_points = await getRightPoints(userId);
+        await db.collection("users").doc(userId).update({
+          right_points,
+        });
+      } catch (e) {
+        functions.logger.log("fallo al actualizar los puntos", e);
+      }
+    };
 
-    await updateBinary()
-})
+    await updateBinary();
+  });
 
-exports.onDeleteRightPoints = functions.firestore.document('users/{userId}/right-points/{sideId}').onDelete(async (snapshot, context) => {
+exports.onDeleteRightPoints = functions.firestore
+  .document("users/{userId}/right-points/{sideId}")
+  .onDelete(async (snapshot, context) => {
     const userId = context.params.userId;
-    let recalculate = 0;
-    const docRef = await db.collection('users').doc(userId).collection('right-points').get()
-    docRef.forEach((d) => {
-        recalculate += d.data().points
-    })
     try {
-        await db.collection('users').doc(userId).update({
-            'right_points': recalculate
-        })
+      const right_points = await getRightPoints(userId);
+      await db.collection("users").doc(userId).update({
+        right_points,
+      });
     } catch (e) {
-        functions.logger.log("fallo al actualizar los puntos", e)
+      functions.logger.log("fallo al actualizar los puntos", e);
     }
-})
+  });
